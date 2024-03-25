943ca209547e09b0d95ea81f557cf7fbfeea0777
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-03-24 21:56:00 -0400

    Complete test for reverseString function

---
+++ tests/reverse_string.test.js
@@ -0,0 +1,13 @@
+import reverseString from "../src/reverse_string";
+
+it("Reverse String", () => {
+  expect(reverseString("Morning")).toBe("gninroM");
+});
+
+it("Reverse String", () => {
+  expect(reverseString("Friday")).toBe("yadirF");
+});
+
+it("Reverse String", () => {
+  expect(reverseString("Love")).toBe("evoL");
+});
